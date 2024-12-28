5d03e56fab6c71c68a7cb62fb1df70f9f70e7535
farzan <farzan.6118@gmail.com>
2024-12-28 20:33:58 +0330

    changes on controller

--- src/main/java/com/example/specification/controller/EmployeeController.java
+++ src/main/java/com/example/specification/controller/EmployeeController.java
@@ -2,8 +2,7 @@ package com.example.specification.controller;
 
 import com.example.specification.model.Employee;
 import com.example.specification.service.EmployeeService;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -15,34 +14,28 @@ import java.util.List;
 public class EmployeeController {
     private final EmployeeService employeeService;
 
-    Logger logger = LoggerFactory.getLogger(EmployeeController.class);
-
     public EmployeeController(EmployeeService employeeService) {
         this.employeeService = employeeService;
     }
 
-    @GetMapping("selectAll")
-    public List<Employee> selectAll() {
-        logger.info("Select all records");
-        return employeeService.selectAll();
+    @GetMapping("employees")
+    public ResponseEntity<List<Employee>> selectAll() {
+        return ResponseEntity.ok(employeeService.selectAll());
     }
 
     @GetMapping("orderBySalary")
-    public List<Employee> orderBySalary() {
-        logger.info("Select all records by 'salary' order");
-        return employeeService.orderBySalary();
+    public ResponseEntity<List<Employee>> orderBySalary() {
+        return ResponseEntity.ok(employeeService.orderBySalary());
     }
 
     @GetMapping("orderById")
-    public List<Employee> orderById() {
-        logger.info("Select all records by 'id' order");
-        return employeeService.orderById();
+    public ResponseEntity<List<Employee>> orderById() {
+        return ResponseEntity.ok(employeeService.orderById());
     }
 
     @GetMapping("orderByIdBiggerThan5")
-    public List<Employee> orderByIdBiggerThan5() {
-        logger.info("Select all records by 'id' bigger than 5");
-        return employeeService.orderByIdBiggerThan5();
+    public ResponseEntity<List<Employee>> orderByIdBiggerThan5() {
+        return ResponseEntity.ok(employeeService.orderByIdBiggerThan5());
     }
 
 
